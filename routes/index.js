c0adc277ff021690dcc94cd116541213236cd9f3
atate13 <atate13@yahoo.com>
2019-05-08 21:10:08 -0400

    pages and other updates

--- routes/index.js
+++ routes/index.js
@@ -7,12 +7,6 @@ const authRoutes = require("./authRoutes");
 router.use("/api", apiRoutes);
 router.use("/auth", authRoutes);
 
-
-router.use(function(req, res) {
-  res.sendFile(path.join(__dirname, "../../client/public/index.html"));
-});
-
-
 // If no API routes are hit, send the React app
 router.use(function (req, res) {
     res.sendFile(path.join(__dirname, '../client/public/index.html'));
